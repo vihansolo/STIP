2a1bdee38fb0d77ab8104457db207947a592e076
Vihang Garud <garudvihang0@gmail.com>
2018-12-14 00:40:34 +0530

    Cleaning up code #1

--- STIPModels/MeasureOfDispersions/StandardDeviation.py
+++ STIPModels/MeasureOfDispersions/StandardDeviation.py
@@ -1,5 +1,4 @@
 from statistics import stdev
-import math
 
 class StandardDeviation() :
 
--- main.py
+++ main.py
@@ -3,28 +3,46 @@ from STIPModels.MeasureOfDispersions import Variance, StandardDeviation, Interqu
 
 inputData = [7, 7, 31, 31, 47, 75, 87, 115, 116, 119, 119, 155, 177]
 
+# Measure of Central Tendencies
+
+# Mean
+
 print()
 meanObject = Mean.Mean()
 print('Mean =',meanObject.calculateMean(inputData))
+
+# Median
 
 print()
 medianObject = Median.Median()
 print('Median =',medianObject.calculateMedian(inputData))
 
+# Mode
+
 print()
 modeObject = Mode.Mode()
+
 try :
     print('Mode =',modeObject.calculateMode(inputData))
+
 except :
     print('Multiple Modes found')
+
+# Measure of Dispersion
+
+# Variance
 
 print()
 varianceObject = Variance.Variance()
 print('Variance =',varianceObject.calculateVariance(inputData))
 
+# Standard Deviation
+
 print()
 standardDeviationObject = StandardDeviation.StandardDeviation()
 print('Standard Deviation =',standardDeviationObject.calculateStandardDeviation(inputData))
+
+# Interquartile Range
 
 print()
 interquartileRangeObject = InterquartileRange.InterquartileRange()
