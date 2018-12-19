4ee0751bdb4f8b12ed156a5aa8978b7934dd9a22
Vihang Garud <garudvihang0@gmail.com>
2018-12-19 11:38:59 +0530

    Added NonLinearRegression.py
    This python file uses dataset from Quandl website belonging to WIKI/GOOGL. This dataset has been used for a reference to represent Non-Linear Regression. This dataset may or may not be changed in the future.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from STIPModels.MeasureOfCentralTendencies import Mean, Median, Mode
 from STIPModels.MeasureOfDispersions import Variance, StandardDeviation, InterquartileRange
-from STIPModels.RegressionAnalysis import LinearRegression
+from STIPModels.RegressionAnalysis import LinearRegression, NonLinearRegression
 
 inputData = [7, 7, 31, 31, 47, 75, 87, 115, 116, 119, 119, 155, 177]
 input2DData = [(1,5), (2,4), (3,6), (4,5), (5,6), (6,7)]
@@ -51,3 +51,10 @@ print('Interquartile Range =',InterquartileRange.InterquartileRange().calculateI
 print()
 print('Linear Regression : ')
 LinearRegression.LinearRegression().calculateLinearRegression(input2DData)
+
+# NonLinear Regression
+
+print()
+print('Non Linear Regression : ')
+print()
+NonLinearRegression.NonLinearRegression().calculateNonLinearRegression()
