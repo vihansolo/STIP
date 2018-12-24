d3a7eb4e5c2d0939a7d59c86e4214143fceed30e
Vihang Garud <garudvihang0@gmail.com>
2018-12-24 22:18:52 +0530

    Added StudentTTest.py

---
+++ STIPModels/HypothesisAnalysis/StudentTTest.py
@@ -0,0 +1,16 @@
+from scipy.stats import ttest_ind
+
+class StudentTTest() :
+
+    def __init__(self) :
+        pass
+
+    def calculateStudentTTest(self, input2DData = []) :
+
+        x = [x for x, y in input2DData]
+        y = [y for x, y in input2DData]
+
+        stat, p = ttest_ind(x, y)
+
+        print('\tStat :',stat)
+        print('\tP :',p)
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from STIPModels.MeasureOfCentralTendencies import Mean, Median, Mode
 from STIPModels.MeasureOfDispersions import Variance, StandardDeviation, InterquartileRange
 from STIPModels.RegressionAnalysis import LinearRegression, NonLinearRegression
 from STIPModels.Forecasting import Forecasting
+from STIPModels.HypothesisAnalysis import StudentTTest
 
 inputData = [7, 7, 31, 31, 47, 75, 87, 115, 116, 119, 119, 155, 177]
 input2DData = [(1,5), (2,4), (3,6), (4,5), (5,6), (6,7)]
@@ -63,4 +64,15 @@ NonLinearRegression.NonLinearRegression().calculateNonLinearRegression()
 # Forecasting
 
 print()
+print('Forecasting :')
+print()
 Forecasting.Forecasting().calculateForecasting()
+
+# Hypothesis Analysis
+
+# Student T-Test
+
+print()
+print('Student T-Test : ')
+print()
+StudentTTest.StudentTTest().calculateStudentTTest(input2DData)
